0b863eb083b263fd736c63db570620e274e0320d
MPK <87748047+kaveeshpandian@users.noreply.github.com>
2023-01-20 19:16:53 +0530

    Create rock_paper_scissors.py

---
+++ rock_paper_scissors.py
@@ -0,0 +1,20 @@
+import random
+
+def play():
+    user = input("whats yours choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r','p','s'])
+
+    if user == computer:
+        return 'tie'
+    if is_win(user, computer):
+        return "you win!"
+    return "you lost! "
+
+
+def is_win(player, opponent):
+    if((player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or
+        (player == 'p' and opponent == 'r')):
+        return True
+
+
+print(play())
